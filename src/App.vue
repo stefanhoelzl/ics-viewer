33c1cf5934814da759672baa1f274aa0a2143d14
Stefan Hoelzl <1478183+stefanhoelzl@users.noreply.github.com>
2022-01-13 16:35:37 +0000

    changed calendar component

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,145 @@
 <template>
-  <v-app id="ics-viewer" v-cloak>
-    <ds-calendar-app ref="app" :read-only="true">
-      <template slot="title"> {{ title }} </template>
-      <template slot="eventPopover" slot-scope="slotData"></template>
-    </ds-calendar-app>
-  </v-app>
+	<div id="app">
+		<div class="calendar-parent">
+			<calendar-view
+				:items="items"
+				:show-date="showDate"
+				:time-format-options="{ hour: 'numeric', minute: '2-digit' }"
+				:enable-drag-drop="false"
+				:disable-past="false"
+				:disable-future="false"
+				:show-times="false"
+				display-period-uom="month"
+				:display-period-count="1"
+				:starting-day-of-week="1"
+				:class="themeClasses"
+				:current-period-label="title"
+				:displayWeekNumbers="true"
+				:enable-date-selection="true"
+				@click-item="onClickItem"
+			>
+				<template #header="{ headerProps }">
+					<calendar-view-header slot="header" :header-props="headerProps" @input="setShowDate" />
+				</template>
+			</calendar-view>
+		</div>
+		<div v-if="message" class="notification is-success">{{ message }}</div>
+	</div>
 </template>
-
 <script>
-import axios from "axios";
-import ICAL from "ical.js";
-import { Parse } from "dayspan";
+import "../node_modules/vue-simple-calendar/dist/style.css"
+import "../node_modules/vue-simple-calendar/static/css/default.css"
+import "../node_modules/vue-simple-calendar/static/css/holidays-us.css"
+
+import axios from "axios"
+import ICAL from "ical.js"
+
+import { CalendarView, CalendarViewHeader, CalendarMath } from "vue-simple-calendar"
 
 export default {
-  name: "app",
+	name: "App",
+	components: {
+		CalendarView,
+		CalendarViewHeader,
+	},
+	data() {
+		return {
+			/* Show the current month, and give it some fake items to show */
+			title: "Calendar",
+			showDate: this.thisMonth(1),
+			message: "",
 
-  data: () => ({
-    title: "Calendar",
-  }),
+			items: [
+				{
+					id: "e0",
+					startDate: "2021-01-14",
+					endDate: "2021-01-20",
+					title: "Multi-day item with a long title and times",
+				},
+			],
+		}
+	},
+	computed: {
+		userLocale() {
+			return CalendarMath.getDefaultBrowserLocale
+		},
+		dayNames() {
+			return CalendarMath.getFormattedWeekdayNames(this.userLocale, "long", 0)
+		},
+		themeClasses() {
+			return {
+				"theme-default": true,
+			}
+		},
+	},
+	mounted() {
+		const urlParams = new URLSearchParams(window.location.search)
+		const ics = urlParams.get("ics") || prompt("Please enter a URL to your ICS/ICAL file")
+		this.title = urlParams.get("title") || prompt("Please enter a Title for your calendar")
+		window.document.title = this.title
+		const search = `?title=${this.title}&ics=${ics}`
+		if (window.location.search != search) {
+			window.location.search = search
+		}
+		const url = "https://larrybolt-cors-anywhere.herokuapp.com/" + ics
+		axios.defaults.headers["Access-Control-Allow-Origin"] = "*"
+		axios.defaults.headers["Content-Type"] = "application/x-www-form-urlencoded"
+		axios.get(url).then((response) => {
+			const vcalendar = new ICAL.Component(ICAL.parse(response.data))
+			this.items = Array.from(vcalendar.getAllSubcomponents("vevent"), (vevent, id) => {
+				var event = new ICAL.Event(vevent)
+				return {
+					id: "event" + id,
+					startDate: event.startDate.toString(),
+					endDate: event.endDate.toString(),
+					title: event.summary,
+				}
+			})
+		})
+	},
 
-  mounted() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const ics =
-      urlParams.get("ics") ||
-      prompt("Please enter a URL to your ICS/ICAL file");
-    this.title =
-      urlParams.get("title") ||
-      prompt("Please enter a Title for your calendar");
-    window.document.title = this.title;
-
-    const search = `?title=${this.title}&ics=${ics}`;
-    if (window.location.search != search) {
-      window.location.search = search;
-    }
-
-    const url = "https://larrybolt-cors-anywhere.herokuapp.com/" + ics;
-    axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
-    axios.defaults.headers["Content-Type"] =
-      "application/x-www-form-urlencoded";
-    axios.get(url).then((response) => {
-      const vcalendar = new ICAL.Component(ICAL.parse(response.data));
-      const events = Array.from(
-        vcalendar.getAllSubcomponents("vevent"),
-        (vevent) => {
-          var event = new ICAL.Event(vevent);
-          var start = Parse.day(event.startDate.toString());
-          return {
-            data: {
-              title: event.summary,
-              color: "#2196F3",
-            },
-            schedule: {
-              month: [start.month],
-              dayOfMonth: [start.dayOfMonth],
-              year: [start.year],
-              duration: event.duration.days,
-              durationUnit: "day",
-            },
-          };
-        }
-      );
-      this.$refs.app.setState({ events: events });
-    });
-  },
-};
+	methods: {
+		thisMonth(d, h, m) {
+			const t = new Date()
+			return new Date(t.getFullYear(), t.getMonth(), d, h || 0, m || 0)
+		},
+		setShowDate(d) {
+			this.showDate = d
+		},
+		onClickItem(e) {
+			this.message = e.title
+		},
+	},
+}
 </script>
 
 <style>
-body,
 html,
-#app,
-#dayspan {
-  font-family: Roboto, sans-serif !important;
-  width: 100%;
-  height: 100%;
+body {
+	height: 100%;
+	margin: 0;
+	background-color: #f7fcff;
 }
 
-.v-btn--flat,
-.v-text-field--solo .v-input__slot {
-  background-color: #f5f5f5 !important;
-  margin-bottom: 8px !important;
+#app {
+	display: flex;
+	flex-direction: column;
+	font-family: Calibri, sans-serif;
+	width: 95vw;
+	min-width: 30rem;
+	max-width: 100rem;
+	min-height: 40rem;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+.calendar-parent {
+	display: flex;
+	flex-direction: column;
+	flex-grow: 1;
+	overflow-x: hidden;
+	overflow-y: hidden;
+	max-height: 100vh;
+	background-color: white;
 }
 </style>
